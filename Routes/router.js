4643e68017ce09609ea7880fccd432e2a0d7aa5c
Athulsn10 <athulnair10@outlook.com>
2024-06-01 16:54:59 +0530

    changes import of getSubjectsBySemester method

--- Routes/router.js
+++ Routes/router.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const courseController = require('../Controllers/subController');
+const {getSubjectsBySemester } = require('../Controllers/subController');
 const {addToFav, getAllFav, removeFromFav} = require('../Controllers/favorites')
 const {protect} = require('../middleware/auth');
 
 
-router.get('/semester/:semester(\\d+)', courseController.getSubjectsBySemester);
+router.get('/semester/:semester(\\d+)',protect, getSubjectsBySemester);
 router.post('/favorites/add',protect,addToFav)
 router.get('/favorites/all',protect,getAllFav)
 router.delete('/favorites/remove',protect,removeFromFav)
